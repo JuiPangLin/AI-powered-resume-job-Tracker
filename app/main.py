0a2ea0581479192f95c43328b4151b67c7453ff1
lin1434 <111909344+JuiPangLin@users.noreply.github.com>
2025-02-24 14:10:25 -0500

    main.py updated

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-from app.routes import resume
-
-
+from app.routes import resume, cover_letter, job_tracker, scraper
 
 app = FastAPI()
+
+# Include API routes
 app.include_router(resume.router, prefix="/resume", tags=["Resume Analysis"])
+app.include_router(cover_letter.router, prefix="/cover_letter", tags=["Cover Letter Generation"])
+app.include_router(job_tracker.router, prefix="/job_tracker", tags=["Job Tracking"])
+app.include_router(scraper.router, prefix="/scraper", tags=["Web Scraping"])
 
 @app.get("/")
 def root():
